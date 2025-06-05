13f14f31bbda61b2a027f9c52772a97185c92d19
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-05 12:19:44 -0500

    forgot to commit on previous style and content added first const to script just now

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 console.log("Hello");
 
 //js object
-const randomIdea = [];//collection of data
+const randomIdea = [
+    "Take 15 minutes to do yoga in a nice setting.";
+    "Grow at least 5 plants vegetables and/or flowers.";
+    "Meditate first thing in the morning or last thing at night.";
+    "Take at least five pics of your favorite outdoor spots";
+    "Skip some stones at your favorite pond or lake."
+];//collection of data
 
 //defining the shape of obj from scratch
 
+const yoga = {};
 
+const gardening = {};
+
+const meditation = {};
+
+const photography = {};
+
+const kayaking = {};
 
 
 

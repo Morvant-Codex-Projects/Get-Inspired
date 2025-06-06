a37ce8c7b6cfbc9dd50609db55c497e10da71538
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-06 11:51:01 -0500

    moved script in index

--- script.js
+++ script.js
@@ -2,37 +2,30 @@ console.log("Hello");
 
 //js object
 const randomIdea = [
-    "Take 15 minutes to do yoga in a nice setting.";
-    "Grow at least 5 plants vegetables and/or flowers.";
-    "Meditate first thing in the morning or last thing at night.";
-    "Take at least five pics of your favorite outdoor spots";
+    "Take 15 minutes to do yoga in a nice setting.",
+    "Grow at least 5 plants vegetables and/or flowers.",
+    "Meditate first thing in the morning or last thing at night.",
+    "Take at least five pics of your favorite outdoor spots.",
     "Skip some stones at your favorite pond or lake."
 ];//collection of data
 
-//defining the shape of obj from scratch- may use later
+console.log("randomIdea");
 
-/*const yoga = {};
+const button = document.getElementById("tryThis");
 
-const gardening = {};
+button.addEventListener("click",displayText);
 
-const meditation = {};
-
-const photography = {};
-
-const kayaking = {};*/
-
-
-// add event listener
-tryThis.addEventListener("onclick",return(randomIdea));
-
-function addEventListener(randomIdea, "onclick"); {
-    e.preventDefault();
-    const tryThis = document.getElementById("tryThis");
-    return(randomIdea)
+function displayText(element){
+   element.displayText="randomIdea"
 
 }
 
-console.log(randomIdea)
 
-addEventListener.getElementById("tryThis").return
+//document.getElementById("tryThis").addEventListener("click", randomIdea);
+
+//function displayDate() {
+  //document.getElementById("randomIdea").innerHTML = Date();
+
+
+
 
